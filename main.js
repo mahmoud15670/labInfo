51aebe9cb29e2ef78c8c7858b099fc7f408a94b5
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2025-12-18 22:02:38 +0000

    Dec 18, 2025, 10:02 PM

--- main.js
+++ main.js
@@ -7,13 +7,22 @@ document.addEventListener("DOMContentLoaded", function () {
   const tubeColor = document.getElementById("tubecolor");
   const container = document.getElementById("container");
   const testNameInput = document.getElementById("query");
+
+  testNameInput.addEventListener("input", function () {
+    if (testNameInput.value.trim() === "") {
+      labInfoDiv.style.display = "none";
+    } else {
+      labInfoDiv.style.display = "block";
+    }
+  });
+
   searchButton.addEventListener("click", function () {
-    testNameInput.value.trim().toLowerCase();
+    const testNameValue = testNameInput.value.trim().toLowerCase();
     fetch("info.json")
       .then((response) => response.json())
       .then((data) => {
-        if (data[testNameInput]) {
-          const testInfo = data[testNameInput];
+        if (data[testNameValue]) {
+          const testInfo = data[testNameValue];
 
           if (testInfo.cond === false) {
             testInfo.cond = "N/A";
