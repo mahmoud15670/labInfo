2baa88a6074cb49f290a456e3093d1e65d11b264
mahmoud15670 <118519725+mahmoud15670@users.noreply.github.com>
2025-12-18 22:36:29 +0000

    Dec 18, 2025, 10:36 PM

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
             key.toLowerCase().includes(inputValue.toLowerCase()) &&
             inputValue !== ""
           ) {
-            suggestions.innerHTML += `<li class="list-group-item" id="${key}" onclick="suggestions.innerHTML = ''">${key}</li>`;
+            suggestions.innerHTML += `<li class="list-group-item" id="${key}" onclick="testNameInput.value='${key}';suggestions.innerHTML = ''">${key}</li>`;
           }
         }
       });
